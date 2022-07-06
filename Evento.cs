e2167470fcd58fbdc657cb59208e42908029395a
IonCatana <catana.ion17@yahoo.it>
2022-07-06 17:20:09 +0200

    getter & setter

--- Evento.cs
+++ Evento.cs
@@ -8,7 +8,56 @@ namespace csharp_gestore_eventi
 {
     internal class Evento
     {
+        private string titoloEvento; //lettura e scrittura
+        private DateTime dataEvento; //lettura e scrittura
+        private int MaxCapienza; //lettura
+        private int nPostiPrenotati; //lettura
 
+        // --- Getter & Setter ---
+        private void SetTitoloEvento(string titolo)
+        {
+            if (titolo == null)
+            {
+                throw new ArgumentNullException(nameof(titolo));
+            }
+            this.titoloEvento = titolo;
+        }
+        public string GetTitoloEvento()
+        {
+            return this.titoloEvento;
+        }
+        public void SetDataEvento(DateTime data)
+        {
+            if (data < DateTime.Now)
+            {
+                throw new InvalidTimeZoneException(nameof(data));
+            }
+            this.dataEvento = data;
+        }
+        public DateTime GetDataEvento()
+        {
+            return this.dataEvento;
+        }
+        private void SetMaxCapienza(int maxCapienza)
+        {
+            if (maxCapienza < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxCapienza));
+            }
+            else
+            {
+                this.MaxCapienza = maxCapienza;
+            }
+        }
+        public int GetMaxCapienza()
+        {
+            return this.MaxCapienza;
+        }
+        public int GetNPostiPrenotati()
+        {
+            return this.nPostiPrenotati;
+        }
 
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_gestore_eventi;
+Console.WriteLine("Tra pocco bestemmio...!");
